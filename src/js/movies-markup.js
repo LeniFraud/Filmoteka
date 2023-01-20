1bbf138d7862e3613e3c324c257a54bdbf130723
KaterynaChernetska <vesnavved@gmail.com>
2023-01-20 11:35:26 +0200

    Update gallery

--- src/js/movies-markup.js
+++ src/js/movies-markup.js
@@ -4,43 +4,13 @@ import TheMovieAPI from './movies-api';
 const container = document.querySelector('.gallery');
 const theMovieAPI = new TheMovieAPI();
 
-// async function  getMovieInfo() {
-//     try{
-//         const movieInfo = await theMovieAPI.fetchTrendingFilms();
-//         console.log(movieInfo)
-       
-
-//     }
-//     catch(error) {
-//         console.error(error);
-//     }
-// }
-
-// async function getGenres() {
-//     try{
-//         const genres = await theMovieAPI.getGenres();
-//         console.log(genres)
-//     }
-//     catch (error){
-//         console.error(error);
-//     }
-// }
-// getMovieInfo();
-// getGenres();
-
 async function getData() {
     try{
         const movieInfo = await theMovieAPI.fetchTrendingFilms();
         const genres = await theMovieAPI.getGenres();
-        console.log(genres);
-        console.log(movieInfo.results);
         movieInfo.results.forEach(film => {
-            // чи краще створити окремий ключ?
-            film.genre_ids = film.genre_ids.map(filmId => genres.find(({id}) => id === filmId))
-            // console.log(film.genre_ids);
-        })
-        
-        
+            film.genre_names = film.genre_ids.map(filmId => genres.find(({id}) => id === filmId)).map(({name}) => name);
+        })        
         container.innerHTML = createMarkup(movieInfo.results);
         
     }
@@ -48,23 +18,30 @@ async function getData() {
         console.log(error);
     }
 }
-getData()
-
-
-
+getData();
 
  function createMarkup(array) {
+    const BASE_URL_FOR_IMAGES = 'https://image.tmdb.org/t/p/w500';
     return array.map(el => 
-`<div class = "movie-card"
-         <img class="movie-image" src="${el.poster_path}" alt="${el.original_title} poster" loading="lazy" /> 
-         <h1 class= "movie-title">${el.original_title}</h1>
-    <div class="movie-info">
+`<div class = "movie-card">
+<div class="movie-image-container">
+         <img class="movie-image" src="${BASE_URL_FOR_IMAGES}${el.poster_path}" alt="${el.original_title} poster" /> 
+         </div>
+         <h1 class= "movie-title">${el. original_title}</h1>
+    <div class="movie-info">   
+     ${makeGenresList (el)}
     <span class = "movie-line"> | </span>
-    <span class = "movie-year"> ${el.release_date}</span>
-    <span class="movie-rate"> ${Math.ceil(el.vote_average)} 
+    <span class = "movie-year"> ${el.release_date.slice(0,4)} </span>
+    <span class="movie-rate"> ${el.vote_average.toFixed(1)} 
     </div>
 </div>
 `).join('')
-
 };
 
+
+function makeGenresList (el) {
+    return  `<div>${el.genre_names.length > 2 ? `<span class ="movie-genre">${el.genre_names[0]}, 
+    </span>` + `<span class="movie-genre">${el.genre_names[1]}, </span>` +
+        `<span class="movie-other">Other </span>` : el.genre_names[0]}
+    </div>`
+}
