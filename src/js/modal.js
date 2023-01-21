ca0789c20f9b9695cc4df69a39d86972ce0128aa
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-21 13:50:07 +0200

    modal updated

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,30 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import TheMovieAPI from './movies-api';
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    container: document.querySelector('.gallery'),
   };
+
+  refs.container.addEventListener('click', getModalData);
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  function getModalData(event) {
+    const movieTitle =
+      event.currentTarget.querySelector('.movie-title').textContent;
+    const movieID = TheMovieAPI.getMovieID(movieTitle).then(
+      response => response.json
+    );
+    console.log(movieID);
   }
 })();
