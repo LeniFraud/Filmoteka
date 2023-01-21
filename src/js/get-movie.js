d058d1a3d1538024043febd5c5b21d43c4f2d3a8
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-21 19:12:27 +0200

    import createMarkup + fix first load wached
    import createMarkup + fix first load wached

--- src/js/get-movie.js
+++ src/js/get-movie.js
@@ -1,4 +1,5 @@
 import TheMovieAPI from './movies-api';
+import { createMarkup, makeGenresList } from './cards-markup';
 
 const theMovieAPI = new TheMovieAPI();
 
@@ -22,6 +23,26 @@ const onContactFormFieldQueue = async event => {
 
 onContactFormFieldQueue();
 
+async function onWachedBtn() {
+  wachedBtn.classList.add('active');
+  queueBtn.classList.remove('active');
+
+  try {
+    const movieInfo = JSON.parse(localStorage.getItem('wached'));
+    const genres = await theMovieAPI.getGenres();
+    movieInfo.results.forEach(film => {
+      film.genre_names = film.genre_ids
+        .map(filmId => genres.find(({ id }) => id === filmId))
+        .map(({ name }) => name);
+    });
+    container.innerHTML = createMarkup(movieInfo.results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onWachedBtn();
+
 const onWachedBtnSubmit = async event => {
   event.preventDefault();
 
@@ -36,7 +57,10 @@ const onWachedBtnSubmit = async event => {
         .map(filmId => genres.find(({ id }) => id === filmId))
         .map(({ name }) => name);
     });
+
+    console.log('Submit Wached')
     container.innerHTML = createMarkup(movieInfo.results);
+
   } catch (error) {
     console.log(error);
   }
@@ -56,7 +80,8 @@ const onQueueBtnSubmit = async event => {
         .map(filmId => genres.find(({ id }) => id === filmId))
         .map(({ name }) => name);
     });
-    container.innerHTML = createMarkup(movieInfo.results);
+    // container.innerHTML = "createMarkup(movieInfo.results)";
+    container.innerHTML = ' ';
   } catch (error) {
     console.log(error);
   }
@@ -64,39 +89,3 @@ const onQueueBtnSubmit = async event => {
 
 wachedBtn.addEventListener('click', onWachedBtnSubmit);
 queueBtn.addEventListener('click', onQueueBtnSubmit);
-
-function createMarkup(array) {
-  const BASE_URL_FOR_IMAGES = 'https://image.tmdb.org/t/p/w500';
-  return array
-    .map(
-      el =>
-        `<div class = "movie-card">
-<div class="movie-image-container">
-         <img class="movie-image" src="${BASE_URL_FOR_IMAGES}${
-          el.poster_path
-        }" alt="${el.original_title} poster" /> 
-         </div>
-         <h1 class= "movie-title">${el.original_title}</h1>
-    <div class="movie-info">   
-     ${makeGenresList(el)}
-    <span class = "movie-line"> | </span>
-    <span class = "movie-year"> ${el.release_date.slice(0, 4)} </span>
-    <span class="movie-rate"> ${el.vote_average.toFixed(1)} 
-    </div>
-</div>
-`
-    )
-    .join('');
-}
-
-function makeGenresList(el) {
-  return `<div>${
-    el.genre_names.length > 2
-      ? `<span class ="movie-genre">${el.genre_names[0]}, 
-    </span>` +
-        `<span class="movie-genre">${el.genre_names[1]}, </span>` +
-        `<span class="movie-other">Other </span>`
-      : el.genre_names[0]
-  }
-    </div>`;
-}
