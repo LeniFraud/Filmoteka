bb6056bea3a4c4c33a2f44b5ba0dd3dda1d66339
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-23 12:17:54 +0200

    -

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,15 +1,44 @@
 import Pagination from 'tui-pagination';
-// import 'tui-pagination/dist/tui-pagination.css'; //fix now styles in html (added with <link>), so I can set my styles above it
+// import 'tui-pagination/dist/tui-pagination.css';
 
-const options = {
-  totalItems: 100,
-  itemsPerPage: 10,
-  visiblePages: 5,
-  page: 1,
-  centerAlign: true,
-};
+import TheMovieAPI from './movies-api';
+const theMovieAPI = new TheMovieAPI();
 
-const container = document.getElementById('tui-pagination-container');
-const instance = new Pagination(container, options);
+//async with no await not ok!!
+async function makePagination({ total_results: totalItems, page }) {
+  const options = {
+    totalItems,
+    page,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    centerAlign: true,
+  };
 
+  const container = document.getElementById('tui-pagination-container');
+  const instance = new Pagination(container, options);
+  return instance;
+}
 // console.log(instance.getCurrentPage());
+
+async function getTotalItems() {
+  try {
+    const data = await theMovieAPI.fetchTrendingFilms();
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// container.addEventListener('click', changePage);
+
+// function changePage() {
+//   const page = instance.getCurrentPage();
+//   theMovieAPI.page = page;
+// }
+
+getTotalItems()
+  .then(data => {
+    makePagination(data);
+  })
+  .catch(err => console.error(err));
