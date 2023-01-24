e00025fbf176e4bbd6400fbb15e8c143fa279027
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-01-24 21:37:10 +0200

    modal style fixed

--- src/js/modal-trailer.js
+++ src/js/modal-trailer.js
@@ -8,10 +8,7 @@ const loader = document.querySelector('.loader');
 
 btnTrailer.addEventListener('click', onTrailerBtnClick);
 
-
-
-
-function onTrailerBtnClick(event) {  
+function onTrailerBtnClick(event) {
   const movieId = getMovieId(event.target);
   modal.classList.remove('is-hidden');
   backdrop.classList.remove('is-hidden');
@@ -22,37 +19,33 @@ function onTrailerBtnClick(event) {
 }
 
 function onBackdropClick() {
-    modal.innerHTML = '';
-    modal.classList.add('is-hidden');
-    backdrop.classList.add('is-hidden');
-  }
-  
+  modal.innerHTML = '';
+  modal.classList.add('is-hidden');
+  backdrop.classList.add('is-hidden');
+}
+
 function onKeyClick(event) {
-   
-    if(event.code !== 'Escape'){
-        return;
-    }
-    modal.innerHTML = '';
-    modal.classList.add('is-hidden');
-    backdrop.classList.add('is-hidden');
-    clearBackdropListeners();
+  if (event.code !== 'Escape') {
+    return;
+  }
+  modal.innerHTML = '';
+  modal.classList.add('is-hidden');
+  backdrop.classList.add('is-hidden');
+  clearBackdropListeners();
 }
 
-
-function clearBackdropListeners(){
-    window.removeEventListener('keydown', onKeyClick);
-    backdrop.removeEventListener('click', onBackdropClick);
+function clearBackdropListeners() {
+  window.removeEventListener('keydown', onKeyClick);
+  backdrop.removeEventListener('click', onBackdropClick);
 }
-    
 
 function getMovieId(target) {
-    const movieCard = target.closest('[data-modal]');
-    if (movieCard === null) {
-      return;
-    }
-    return movieCard.dataset.filmid;
+  const movieCard = target.closest('[data-modal]');
+  if (movieCard === null) {
+    return;
   }
-
+  return movieCard.dataset.filmid;
+}
 
 async function getData(movieId) {
   try {
@@ -70,17 +63,32 @@ async function getData(movieId) {
 function createModalMarkUp(obj) {
   const markUp = `<iframe class="trailer"
     src="https://www.youtube.com/embed/${obj.key}" 
-    title="YouTube video player" frameborder="0"
-     allow="accelerometer; autoplay; clipboard-write;
-      encrypted-media; gyroscope; picture-in-picture;
-       web-share" allowfullscreen></iframe>
+    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 `;
   return markUp;
 }
 
+function clearBackdropListeners() {
+  window.removeEventListener('keydown', onKeyClick);
+  backdrop.removeEventListener('click', onBackdropClick);
+}
 
- function clearBackdropListeners(){
+//  import TheMovieAPI from './movies-api';
 
-window.removeEventListener('keydown', onKeyClick);
-    backdrop.removeEventListener('click', onBackdropClick);
- }
+//  const theMovieAPI = new TheMovieAPI();
+//  const backdrop = document.querySelector('.backdrop-trailer');
+//  const modal = document.querySelector('.trailer-modal');
+//  const btnTrailer = document.querySelector('.trailer-btn');
+//  const loader = document.querySelector('.loader');
+
+//  btnTrailer.addEventListener('click', onTrailerBtnClick);
+
+//  function onTrailerBtnClick(event) {
+//    const movieId = getMovieId(event.target);
+//    modal.classList.remove('is-hidden');
+//    backdrop.classList.remove('is-hidden');
+
+//    getData(movieId);
+//    window.addEventListener('keydown', onKeyClick);
+//    backdrop.addEventListener('click', onBackdropClick);
+//  }
