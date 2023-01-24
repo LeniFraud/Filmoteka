9e3564e92aa27fd5ee3e1a11b76ed17911535025
KaterynaChernetska <vesnavved@gmail.com>
2023-01-24 14:34:16 +0200

    Undate function makeGenresList, fixed bag

--- src/js/get-movie.js
+++ src/js/get-movie.js
@@ -1,5 +1,5 @@
 import TheMovieAPI from './movies-api';
-// import { createMarkup, makeGenresList } from './cards-markup';
+import { createMarkup, makeGenresList } from './cards-markup';
 import { getMovieId } from './modal';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -84,7 +84,7 @@ queueBtn.addEventListener('click', onQueueBtnSubmit);
 async function get(id) {
   try {
     const result = await theMovieAPI.fetchOneFilm(id);
-
+    console.log(result.data);
     container.insertAdjacentHTML('beforeend', createMarkup(result.data));
   } catch (error) {
     console.log(error);
@@ -103,6 +103,7 @@ function createMarkup(movie) {
          </div>
          <h1 class= "movie-title">${original_title}</h1>
     <div class="movie-info">
+    ${makeGenresList(movie)}
     <span class = "movie-line"> | </span>
     <span class = "movie-year"> ${release_date.slice(0, 4)} </span>
     <span class="movie-rate"> ${vote_average.toFixed(1)}
