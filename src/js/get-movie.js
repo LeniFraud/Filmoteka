6295d59eaf3f7046ef38d47a4c3efe213ab71595
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-24 04:53:51 +0200

    fix get-movie

--- src/js/get-movie.js
+++ src/js/get-movie.js
@@ -1,90 +1,118 @@
 import TheMovieAPI from './movies-api';
-import { createMarkup, makeGenresList } from './cards-markup';
+// import { createMarkup, makeGenresList } from './cards-markup';
+import { getMovieId } from './modal';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  position: 'center-top',
+  clickToClose: true,
+  pauseOnHover: false,
+  cssAnimationStyle: 'from-top',
+});
 
 const theMovieAPI = new TheMovieAPI();
 
-const wachedBtn = document.querySelector('.wached');
+const watchedBtn = document.querySelector('.watched');
 const queueBtn = document.querySelector('.queue');
 const container = document.querySelector('.gallery');
 
-const onContactFormFieldWached = async event => {
-  const movieInfo = await theMovieAPI.fetchTrendingFilms();
-
-  localStorage.setItem('wached', JSON.stringify(movieInfo));
-};
-
-onContactFormFieldWached();
-
-const onContactFormFieldQueue = async event => {
-  const movieInfo = await theMovieAPI.fetchTrendingFilms();
-
-  localStorage.setItem('queue', JSON.stringify(movieInfo));
-};
-
-onContactFormFieldQueue();
-
-async function onWachedBtn() {
-  wachedBtn.classList.add('active');
+async function onWatchedBtn() {
+  watchedBtn.classList.add('active');
   queueBtn.classList.remove('active');
+  watchedBtn.disabled = true;
+  queueBtn.disabled = false;
 
+  const moviesArray = JSON.parse(localStorage.getItem('watched'));
+
+  if (moviesArray === null || moviesArray.length === 0) {
+    Notify.failure('You don`t have any watched film');
+    container.innerHTML = 'Your watchlist is empty. Please add some film.';
+    return
+  }
+
+  moviesArray.map(film => {
+    get(film);
+  });
+}
+onWatchedBtn();
+
+function onWatchedBtnSubmit() {
+  watchedBtn.classList.add('active');
+  queueBtn.classList.remove('active');
+  watchedBtn.disabled = true;
+  queueBtn.disabled = false;
+  container.innerHTML = '';
+
+  const moviesArray = JSON.parse(localStorage.getItem('watched'));
+
+  if (moviesArray === null || moviesArray.length === 0) {
+    Notify.failure('You don`t have any watched film');
+    container.innerHTML = 'Your watchlist is empty. Please add some film.';
+    return
+  }
+
+  moviesArray.map(film => {
+    get(film);
+  });
+}
+
+const onQueueBtnSubmit = async event => {
+  event.preventDefault();
+  queueBtn.classList.add('active');
+  watchedBtn.classList.remove('active');
+  watchedBtn.disabled = false;
+  queueBtn.disabled = true;
+  container.innerHTML = '';
+
+  const moviesArray = JSON.parse(localStorage.getItem('queue'));
+
+  if (moviesArray === null || moviesArray.length === 0) {
+    Notify.failure('You don`t have any film in your queue');
+    container.innerHTML = 'Your queue is empty. Please add some film.';
+    return
+  }
+
+  moviesArray.map(film => {
+    get(film);
+  });
+};
+
+watchedBtn.addEventListener('click', onWatchedBtnSubmit);
+queueBtn.addEventListener('click', onQueueBtnSubmit);
+
+async function get(id) {
   try {
-    const movieInfo = JSON.parse(localStorage.getItem('wached'));
-    const genres = await theMovieAPI.getGenres();
-    movieInfo.results.forEach(film => {
-      film.genre_names = film.genre_ids
-        .map(filmId => genres.find(({ id }) => id === filmId))
-        .map(({ name }) => name);
-    });
-    container.innerHTML = createMarkup(movieInfo.results);
+    const result = await theMovieAPI.fetchOneFilm(id);
+
+    container.insertAdjacentHTML('beforeend', createMarkup(result.data));
   } catch (error) {
     console.log(error);
   }
 }
 
-onWachedBtn();
+function createMarkup(movie) {
+  const BASE_URL_FOR_IMAGES = 'https://image.tmdb.org/t/p/w500';
+  const { id, poster_path, original_title, release_date, vote_average } = movie;
+  return `<div class = "movie-card" data-filmId = ${id}>
+        <div class="movie-image-container">
+         <img class="movie-image" src="${checkMoviePoster(
+           BASE_URL_FOR_IMAGES,
+           poster_path
+         )}" alt="${original_title} poster" />
+         </div>
+         <h1 class= "movie-title">${original_title}</h1>
+    <div class="movie-info">
+    <span class = "movie-line"> | </span>
+    <span class = "movie-year"> ${release_date.slice(0, 4)} </span>
+    <span class="movie-rate"> ${vote_average.toFixed(1)}
+    </div>
+</div>
+`;
+}
 
-const onWachedBtnSubmit = async event => {
-  event.preventDefault();
-
-  wachedBtn.classList.add('active');
-  queueBtn.classList.remove('active');
-
-  try {
-    const movieInfo = JSON.parse(localStorage.getItem('wached'));
-    const genres = await theMovieAPI.getGenres();
-    movieInfo.results.forEach(film => {
-      film.genre_names = film.genre_ids
-        .map(filmId => genres.find(({ id }) => id === filmId))
-        .map(({ name }) => name);
-    });
-
-    console.log('Submit Wached');
-    container.innerHTML = createMarkup(movieInfo.results);
-  } catch (error) {
-    console.log(error);
+function checkMoviePoster(baseUrl, posterUrl) {
+  if (posterUrl === null) {
+    return 'https://via.placeholder.com/350x500?text=No+Poster';
   }
-};
-
-const onQueueBtnSubmit = async event => {
-  event.preventDefault();
-
-  queueBtn.classList.add('active');
-  wachedBtn.classList.remove('active');
-
-  try {
-    const movieInfo = JSON.parse(localStorage.getItem('queue'));
-    const genres = await theMovieAPI.getGenres();
-    movieInfo.results.forEach(film => {
-      film.genre_names = film.genre_ids
-        .map(filmId => genres.find(({ id }) => id === filmId))
-        .map(({ name }) => name);
-    });
-    // container.innerHTML = "createMarkup(movieInfo.results)";
-    container.innerHTML = ' ';
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-wachedBtn.addEventListener('click', onWachedBtnSubmit);
-queueBtn.addEventListener('click', onQueueBtnSubmit);
+  return baseUrl + posterUrl;
+}
