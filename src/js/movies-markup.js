9f76a1e46dd38756859fbd1d7adf842cb190eede
KaterynaChernetska <vesnavved@gmail.com>
2023-01-24 14:34:16 +0200

    Undate function makeGenresList, fixed bag

--- src/js/movies-markup.js
+++ src/js/movies-markup.js
@@ -3,13 +3,12 @@ import { createMarkup } from './cards-markup';
 
 const container = document.querySelector('.gallery');
 export const theMovieAPI = new TheMovieAPI();
-const loader = document.querySelector('.loader');
-const loaderContainer = document.querySelector('.movies-gallery');
+const loader = document.querySelector(".loader");
+
 
 export async function getData() {
   try {
-    loaderContainer.style.height = '350px';
-    loader.style.display = 'block';
+    loader.style.display = "block";
     const movieInfo = await theMovieAPI.fetchTrendingFilms();
     const genres = await theMovieAPI.getGenres();
     movieInfo.results.forEach(film => {
@@ -20,45 +19,10 @@ export async function getData() {
     container.innerHTML = createMarkup(movieInfo.results);
   } catch (error) {
     console.log(error);
-  } finally {
-    loaderContainer.style.height = 'auto';
+  }
+  finally{
     loader.style.display = 'none';
   }
 }
 getData();
 
-/*
- function createMarkup(array) {
-    const BASE_URL_FOR_IMAGES = 'https://image.tmdb.org/t/p/w500';
-    return array.map(el => 
-`<div class = "movie-card">
-<div class="movie-image-container">
-         <img class="movie-image" src="${BASE_URL_FOR_IMAGES}${
-          el.poster_path
-        }" alt="${el.original_title} poster" /> 
-         </div>
-         <h1 class= "movie-title">${el.original_title}</h1>
-    <div class="movie-info">   
-     ${makeGenresList(el)}
-    <span class = "movie-line"> | </span>
-    <span class = "movie-year"> ${el.release_date.slice(0, 4)} </span>
-    <span class="movie-rate"> ${el.vote_average.toFixed(1)} 
-    </div>
-</div>
-`
-    )
-    .join('');
-}
-
-function makeGenresList(el) {
-  return `<div>${
-    el.genre_names.length > 2
-      ? `<span class ="movie-genre">${el.genre_names[0]}, 
-    </span>` +
-        `<span class="movie-genre">${el.genre_names[1]}, </span>` +
-        `<span class="movie-other">Other </span>`
-      : el.genre_names[0]
-  }
-    </div>`;
-}
-*/
